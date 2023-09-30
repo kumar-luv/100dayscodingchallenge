45617c722d031c615a572f196a303164b04cea4e
Lokesh kumar <85893636+kumar-luv@users.noreply.github.com>
2023-09-30 21:43:36 +0530

    Linked list

---
+++ middleOfLinked.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    Node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }cout<<endl;
+}
+
+void printMiddle(Node * head){
+    if(head==NULL)return;
+    Node *slow=head,*fast=head;
+    while(fast!=NULL&&fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    cout<<slow->data;
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	head->next->next->next=new Node(40);
+	head->next->next->next->next=new Node(50);
+	printlist(head);
+	cout<<"Middle of Linked List: ";
+	printMiddle(head);
+	return 0;
+} 
